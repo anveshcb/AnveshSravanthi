7650b2dcab61fb027635421732878f73ff9749e1
anvekuma <anveshkumar.sayana@brambles.com>
2023-08-29 17:06:43 +0530

    Label names change and CSS

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
 
       <div className="container about borderradius backgroundreceptionimage">
           <h2 className="main-title text-center">Reception Invitation</h2>
-          <button class="custom-button" onClick={receptionLocation}>Reception Location</button><br/> <br/>
+          <button class="custom-button" onClick={receptionLocation}>Click here for Reception Location</button><br/> <br/>
           <i className="center"> on September 4th 2023 from 07:00 PM </i>
           <br/><br/>
           <img alt="reception" src="img/Reception.jpeg" className="reception-logo" />
--- src/components/Services.js
+++ src/components/Services.js
@@ -9,8 +9,8 @@ function Services() {
     <div className="container services borderradius backgroundmarriageimage">
       <br/><br/><br/><br/>
         <h2 className="main-title text-center"><i>Marriage Invitation</i></h2>
-        <button class="custom-button" onClick={marriageLocation}>Marriage Location</button><br/> <br/>
-        <button class="custom-button-2" onClick={liveStreaming}>Live Streaming</button><br/> <br/>
+        <button class="custom-button" onClick={marriageLocation}>Click here for Marriage Location</button><br/> <br/>
+        <button class="custom-button-2" onClick={liveStreaming}>Click here for Live Streaming</button><br/> <br/>
         <i className="center"> on September 3rd 2023 @ 10:39 AM </i>
         <br/><br/>
         <img alt="marriage" src="img/Marriage.jpg" className="marriage-logo" />
